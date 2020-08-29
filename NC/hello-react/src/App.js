317d2a7287fef085274571b9890a60d8c65149c0
Sigit-Wasis <sigitghoticmetal2001@gmail.com>
2020-08-29 09:22:32 +0700

    tes

---
+++ NC/hello-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Rendering Element React JS!</h1>
+      <h1>Belajar Buat Masa Depan!</h1>
+      <h2>Hobi Ku Berbayar Bro</h2>
+    </div>
+  );
+}
+
+export default App;
